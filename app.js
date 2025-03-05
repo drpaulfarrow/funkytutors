6ad7bc63b5aea79854586e93f5978bc7b4cd9208
Paul Farrow <paulandrewfarrow@gmail.com.com>
2025-03-05 02:03:16 +0000

    Fixed app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static("public")); // Serve static files (optional)
+// Serve static files (CSS, images, etc.) from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
 
+// Route to serve index.html
 app.get("/", (req, res) => {
-    res.send("Hello from Funky Tutors!");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
